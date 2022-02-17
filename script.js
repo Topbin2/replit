31c9483b45b59d58917e04f4234b6b627ebefe75
상빈 <95295766+Topbin2@users.noreply.github.com>
2022-02-17 13:35:17 +0000

    JS: for

--- script.js
+++ script.js
@@ -35,8 +35,41 @@ var day = '26';
 var hour = '11';
 var minute = '34';
 var second = '27';
-
-
 var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
 console.log(result);
+var today = second.concat('일 입니다.');
+console.log(today);
 
+for ( var i = 1; i <= 5; i++) {
+  console.log('*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 5; i > 0; i--) {
+  console.log('*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 0; i < 11; i += 2) {
+  console.log('*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 10; i > 0; i -= 2) {
+  console.log('*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 1; i <= 16; i *= 2) {
+  console.log('*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 1; i <= 5; i++) {
+  console.log(' '.repeat(5 - i) + '*'.repeat(i));
+}
+console.log('\n');
+
+for ( var i = 5; i >= 1; i--) {
+  console.log(' '.repeat(5 - i) + '*'.repeat(i));
+}
