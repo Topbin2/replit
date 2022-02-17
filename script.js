496e4db8bcda428ac8ef4646a05695f73d343718
상빈 <95295766+Topbin2@users.noreply.github.com>
2022-02-17 11:33:09 +0000

    JS: for

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ console.log(arr);
 
 var arr = [100, 200, 300];
 console.log(typeof(arr));
+
+var a = 2.22;
+console.log(typeof(a));
+
+var a = 10;
+var b = 2;
+for(var i = 1; i < 5; i+=2) {
+  a += 1;
+}
+console.log(a+b);
