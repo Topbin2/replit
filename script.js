2caa62b04547fa1da10f203563ec014b0a12f033
상빈 <95295766+Topbin2@users.noreply.github.com>
2022-02-17 11:50:02 +0000

    JS: concat

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ for(var i = 1; i < 5; i+=2) {
   a += 1;
 }
 console.log(a+b);
+
+var d = {
+  'height' : 100,
+  'weight' : 78,
+  'weight' : 84,
+  'temperature' : 36,
+  'eyesight' : 1
+}
+console.log(d['weight']);
+
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+console.log(result);
