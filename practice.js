26c15723bab2a4aef04b3e5fde9ce39ffade3243
상빈 <95295766+Topbin2@users.noreply.github.com>
2022-03-07 01:49:48 +0000

    JS: practice

--- practice.js
+++ practice.js
@@ -48,40 +48,69 @@
 
 
 
-class UserStorage {
-  loginUser (id, password) {
-    return new Promise ((resolve, reject) => {
-      setTimeout(()=> {
-      if (
-          (id === 'ellie' && password === 'dream') ||
-          (id === 'coder' && password === 'academy')
-        ) {
-          resolve(id);
-        } else {
-          reject(new Error('not found'));
-        }
-      }, 2000);
-    });
-  }
+// class UserStorage {
+//   loginUser (id, password) {
+//     return new Promise ((resolve, reject) => {
+//       setTimeout(()=> {
+//       if (
+//           (id === 'ellie' && password === 'dream') ||
+//           (id === 'coder' && password === 'academy')
+//         ) {
+//           resolve(id);
+//         } else {
+//           reject(new Error('not found'));
+//         }
+//       }, 2000);
+//     });
+//   }
 
-  getRoles (user) {
-    return new Promise ((resolve, reject) => {
-      setTimeout( ()=> {
-      if (user === 'ellie') {
-        resolve({ name: 'ellie', role: 'admin'});
-      } else {
-        reject(new Error('no access'));
-      }
-    }, 1000);  
-    });
-  }
-}
+//   getRoles (user) {
+//     return new Promise ((resolve, reject) => {
+//       setTimeout( ()=> {
+//       if (user === 'ellie') {
+//         resolve({ name: 'ellie', role: 'admin'});
+//       } else {
+//         reject(new Error('no access'));
+//       }
+//     }, 1000);  
+//     });
+//   }
+// }
 
-const userStorage = new UserStorage();
-const id = prompt('enter your id');
-const password = prompt('enter your password');
-userStorage
-  .loginUser(id, password)
-  .then(userStorage.getRoles)
-  .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
-  .catch(console.log)
+// const userStorage = new UserStorage();
+// const id = prompt('enter your id');
+// const password = prompt('enter your password');
+// userStorage
+//   .loginUser(id, password)
+//   .then(userStorage.getRoles)
+//   .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+//   .catch(console.log)
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers);
+
+// const person = {
+//   name: 'Max'
+// };
+
+// const newPerson = {
+//   ...person,
+//   age: 29
+// };
+
+// console.log(newPerson);
+
+// const numbers = [1, 2, 3];
+// [num1, ,num3] = numbers;
+// console.log(num1, num3);
+
+const person = {
+  name: 'max'
+};
+
+const newPerson = {
+  ...person
+};
+person.name = 'sangbin';
+console.log(newPerson);
