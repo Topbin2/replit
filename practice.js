78e0a529e158e6d4c874d890862d328475a8b763
상빈 <95295766+Topbin2@users.noreply.github.com>
2022-03-07 03:59:20 +0000

    JS: practice

--- practice.js
+++ practice.js
@@ -105,12 +105,57 @@
 // [num1, ,num3] = numbers;
 // console.log(num1, num3);
 
-const person = {
-  name: 'max'
-};
+// const person = {
+//   name: 'max'
+// };
 
-const newPerson = {
-  ...person
-};
-person.name = 'sangbin';
-console.log(newPerson);
+// const newPerson = {
+//   ...person
+// };
+// person.name = 'sangbin';
+// console.log(newPerson);
+
+// const array = [1, 2, 3, 4, 5];
+// const newArray = array.map( num => {
+//   return num + 1;
+// })
+// console.log(array);
+// console.log(newArray);
+
+// const array = [1, 200, 150, 400, 350];
+// const found = array.findIndex( num => {
+//   return num > 150;
+// });
+// console.log(found);
+
+// const array = [1, 2, 3, 4, 5];
+// const found = array.findIndex( num => {
+//   return num > 3;
+// });
+// console.log(found);
+
+// const array = [200, 100, 500, 300, 400];
+// const newArray = array.filter( num => {
+//   return num > 300;
+// });
+// console.log(newArray);
+
+// const array = [1, 2, 3, 4, 5];
+// const newArray = array.reduce((acc, cur) => {
+//   return acc + cur;
+// },0);
+// console.log(newArray);
+
+// const array = [1, 2, 3];
+// const newArray = array.concat(100, 200, 300);
+// console.log(array);
+
+const array = ['A', 'B', 'C', 'D', 'E'];
+const newArray = array.slice(2);
+console.log(array);
+console.log(newArray);
+
+// const array = ['개', '고양이', '사자', '호랑이', '공룡'];
+// const newArray = array.splice(2);
+// console.log(array);
+// console.log(newArray);
